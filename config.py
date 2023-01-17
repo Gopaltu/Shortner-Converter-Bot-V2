5a723d5fab39dc05b5d4411e8e00a9d69aafa422
Gopaltu <122040358+Gopaltu@users.noreply.github.com>
2023-01-17 18:37:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
 DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
-ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append(Id Owned Id)
+OWNER_ID =  int(os.environ.get("OWNER_ID", "1293572362")) 
+ADMINS.append("1293572362") if OWNER_ID not in ADMINS else []
+ADMINS.append("1293572362")
 #  Optionnal variables
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
 UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel User name Without @") # For Force Subscription
