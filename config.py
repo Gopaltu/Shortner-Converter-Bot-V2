4eb3415d827a5fd24f364b62ffea102603a15037
Gopaltu <122040358+Gopaltu@users.noreply.github.com>
2023-01-17 18:57:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_HASH = os.environ.get("API_HASH", "Your Api Hash")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
 ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "dstamil")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "gopal")
 DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
 OWNER_ID =  int(os.environ.get("OWNER_ID", "1293572362")) 
 ADMINS.append("1293572362") if OWNER_ID not in ADMINS else []
